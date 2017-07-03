0de85ce74c609a553407dc1792961fae330003e3
Daniel Enge <daniel.enge@zippelmedia.com>
2017-07-03 09:59:58 +0200

    Minor changes

--- src/main/java/com/example/praktikum1/HelloWorldApplication.java
+++ src/main/java/com/example/praktikum1/HelloWorldApplication.java
@@ -6,6 +6,6 @@ package main.java.com.example.praktikum1;
 public class HelloWorldApplication {
     public static void main ( String[] args )
     {
-        System.out.println("Hello World!") ;
+        System.out.println("Hello World Test!") ;
     }
 }
