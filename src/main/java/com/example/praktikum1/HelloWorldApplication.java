2d5e47ab29607cbe5d177324851024cbef243505
derdelfin <tobias.dolfen@me.com>
2017-07-03 09:52:57 +0200

    First commit

---
+++ src/main/java/com/example/praktikum1/HelloWorldApplication.java
@@ -0,0 +1,11 @@
+package main.java.com.example.praktikum1;
+
+/**
+ * Created by tdolfen on 03.07.2017.
+ */
+public class HelloWorldApplication {
+    public static void main ( String[] args )
+    {
+        System.out.println("Hello World!") ;
+    }
+}
